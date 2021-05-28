60c78eecf6b553df6158388899f2866d679963ab
mccullen <mccullen_j@lynchburg.edu>
2021-05-28 16:08:54 -0400

    Work on adding tbi annotator

--- src/main/java/icapa/ae/TBIAnnotator.java
+++ src/main/java/icapa/ae/TBIAnnotator.java
@@ -1,17 +1,54 @@
 package icapa.ae;
 
+import org.apache.ctakes.core.util.annotation.IdentifiedAnnotationBuilder;
+import org.apache.ctakes.core.util.annotation.SemanticGroup;
+import org.apache.ctakes.core.util.regex.RegexSpanFinder;
 import org.apache.ctakes.typesystem.type.refsem.OntologyConcept;
+import org.apache.ctakes.typesystem.type.textsem.DiseaseDisorderMention;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
+
+import java.util.function.Function;
 
 public class TBIAnnotator extends JCasAnnotator_ImplBase {
 
+    public static final String PARAM_REGEX = "Regex";
+    @ConfigurationParameter(
+        name = PARAM_REGEX,
+        description = "Regular expression to use"
+    )
+    private String _regex;
+
+    public static final String PARAM_CUI = "Cui";
+    @ConfigurationParameter(
+        name = PARAM_CUI
+    )
+    private String _cui;
+
+    public static final String PARAM_CODING_SCHEME = "CodingScheme";
+    @ConfigurationParameter(
+        name = PARAM_CODING_SCHEME
+    )
+    private String _codingScheme;
+
+    public static final String PARAM_CODE = "Code";
+    @ConfigurationParameter(
+        name = PARAM_CODE
+    )
+    private String _code;
 
     @Override
     public void process(JCas jCas) {
-        IdentifiedAnnotation identifiedAnnotation = new IdentifiedAnnotation(jCas);
+        try (RegexSpanFinder finder = new RegexSpanFinder("TBI")) {
+            finder.findSpans(jCas.getDocumentText()).forEach(span -> {
 
+            });
+            Function<JCas, ? extends IdentifiedAnnotation> f = DiseaseDisorderMention::new;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
